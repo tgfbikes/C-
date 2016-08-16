8c7f30f180111fa1cf0a2eb0b4d854a352fc6cc9
Stephen King <Stephen.King@Centeva.com>
2016-08-15 20:28:37 -0600

    gradebook running

---
+++ gradeBook/gradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine("Average: " + stats.AverageGrade);
+            Console.WriteLine("Lowest: " + stats.LowestGrade);
+            Console.WriteLine("Highest: " + stats.HighestGrade);
+        }
+    }
+}
